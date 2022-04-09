de59fcf3f7ce6ad7e3d4fb1af64a0f9b64d54fcc
mpuchkova <puchkovamm2001@mail.ru>
2022-04-09 15:55:04 +0300

    task-inheritance

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -8,20 +8,40 @@ interface IStudentData extends IPersonData {
     phone: string;
 }
 
-/*
-1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IPersonData
-2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IPersonData
- */
 export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+
+    constructor(data: IPersonData) {
+        this._name = data.name;
+        this._secondName = data.secondName;
+        this._age = data.age;
+    }
+
+    getData(): IPersonData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
 
-/*
-1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IStudentData
-2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IStudentData
- */
 export class Student extends Person {
     readonly _phone: string;
+
+    constructor(data: IStudentData) {
+        super(data);
+        this._phone = data.phone;
+    }
+
+    getData(): IStudentData {
+        return {
+            phone: this._phone,
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
